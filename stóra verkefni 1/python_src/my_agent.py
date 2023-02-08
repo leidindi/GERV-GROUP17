5bc6befea9f1fe70499e3ac04ae25d1f6b8b94b4
Ingólfur Ari Jóhannsson <ingoar97@gmail.com>
2023-02-08 17:56:28 +0000

    beinagrind fyrir verkefnið

--- stóra verkefni 1/python_src/my_agent.py
+++ stóra verkefni 1/python_src/my_agent.py
@@ -1,8 +1,79 @@
 from agent import Agent
 from environment import *
-#prufa git
-#prufum ennþá meira
-#prufa meira
+
+
+class GeneralNode:
+    def __init__(self, heuristic=0, node_lis=None, alpha=-float('inf'), beta=float('inf'), parent=None,move=None,state=None):
+        self.alpha = alpha
+        self.beta = beta
+        self.parent = parent
+        self.move = move
+        self.best = None
+        self.heuristic = heuristic
+        self.state = state
+        self.children = []
+
+def minimax_ab(node, depth):
+    if depth == 0 or node is None:
+        return node.heuristic
+
+    maximizing_player = depth % 2 == 0
+    if maximizing_player:
+        best_value = -float('inf')
+        children = env.get_legal_moves(node.state)
+        for child in children:
+            value = minimax_ab(child, depth-1, False)
+            best_value = max(best_value, value)
+            node.alpha = max(node.alpha, best_value)
+            if node.beta <= node.alpha:
+                break
+        return best_value
+
+    else:
+        best_value = float('inf')
+        children = env.get_legal_moves(node.state)
+        for child in children:
+            value = minimax_ab(child, depth-1, True)
+            best_value = min(best_value, value)
+            node.beta = min(node.beta, best_value)
+            if node.beta <= node.alpha:
+                break
+        return best_value
+
+class GeneralTree:
+    def __init__(self, limit = 5, depth = 0, state = None):
+        self.root = None
+        self.limit = limit
+        self.depth = depth
+        self.state = state
+        if state == None:
+            raise EnvironmentError
+    def populate_tree(self):
+        self.root = self.populate_tree_recur()
+
+    def populate_tree_recur(self):
+        data = input("input value : ")
+        if data == "":
+            return ""
+        node = GeneralNode(data)
+        for move in env.
+            child = self.populate_tree_recur()
+            if child == "":
+                break
+            node.node_lis.append(child)
+        return node
+
+    def alpha_beta(self):
+        if self.depth == self.limit:
+            return None
+
+    def alphabeta_root(self):
+        under_root = []
+        for x in env.get_legal_moves(self.state):
+            under_root.append()
+
+        return self.alpha_beta()
+
 class MyAgent(Agent):
     def __init__(self):
         self.role = None
@@ -12,15 +83,10 @@ class MyAgent(Agent):
         self.height = 0
         self.env = None
 
-    
-    def alpha_beta(self):
-        return True
-
-    def alphabeta_root(self):
-        return self.alpha_beta()
-
-    def get_best_move(self):
-        return self.alphabeta_root()
+    def get_best_move(self,limit=5):
+        tree = GeneralTree(state=self.env.current_state,limit=limit)
+        tree.alphabeta_root()
+        return tree.root.best
 
 
     # start() is called once before you have to select the first action. Use it to initialize the agent.
@@ -43,7 +109,6 @@ class MyAgent(Agent):
             else:
                 last_player = 'black'
             print("%s moved from %s to %s" % (last_player, str(last_action[0:2]), str(last_action[2:4])))
-            # TODO: 1. update your internal world model according to the action that was just executed
             self.env.move(self.env.current_state, last_action)
         else:
             print("first move!")
@@ -51,7 +116,6 @@ class MyAgent(Agent):
         # update turn (above that line it myTurn is still for the previous state)
         self.my_turn = not self.my_turn
         if self.my_turn:
-            # TODO: 2. run alpha-beta search to determine the best move
             x1, y1, x2, y2 = self.get_best_move()
             return "(move " + " ".join(map(str, [x1, y1, x2, y2])) + ")"
             # send the move first, then update your own world
